ee8a1a906ae98f669218345acb329e16b3aa08bc
Kayce Basques <kayce@google.com>
2025-05-19 15:38:35 -0700

    sphinxdocs: support persistent workers

--- sphinxdocs/private/sphinx_build.py
+++ sphinxdocs/private/sphinx_build.py
@@ -1,8 +1,29 @@
+import argparse
 import os
 import pathlib
 import sys
 
 from sphinx.cmd.build import main
 
+
+def _create_parser() -> "argparse.Namespace":
+    parser = argparse.ArgumentParser(fromfile_prefix_chars="@")
+    parser.add_argument("--persistent_worker", action="store_true")
+    return parser
+
+
+JsonWorkerRequest = object
+JsonWorkerResponse = object
+
+
+def _main(args: "list[str]") -> int:
+    options = _create_parser().parse_args(args)
+    if options.persistent_worker:
+        main(["python3", "-m", "sphinx", "build", "-M", "man")
+    else:
+        main()
+    return 0
+
+
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(_main(sys.argv[1:]))
