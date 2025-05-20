d76d2f888e24ba277bf621d29d06174246f4d6f9
Kayce Basques <kayce@google.com>
2025-05-20 13:18:35 -0700

    Add logging

--- sphinxdocs/private/sphinx_build.py
+++ sphinxdocs/private/sphinx_build.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import logging
 import os
 import pathlib
 import sys
@@ -18,30 +19,37 @@ class _PersistentWorker:
     def __init__(self, instream: "typing.TextIO", outstream: "typing.TextIO"):
         self._instream = instream
         self._outstream = outstream
+        self._logger = logging.getLogger("sphinx_build")
+        self._logger.info("starting persistent worker")
 
     def run(self) -> None:
-        while True:
-            request = None
-            try:
-                request = self._get_next_request()
-                if request is None:
-                    break
-                response = self._process_request(request)
-                if response:  # May be none for cancel request
-                    self._send_response(response)
-            except Exception:
-                output = (
-                    f"Unhandled error:\nRequest: {request}\n"
-                    + traceback.format_exc()
-                )
-                request_id = 0 if not request else request.get("requestId", 0)
-                self._send_response(
-                    {
-                        "exitCode": 3,
-                        "output": output,
-                        "requestId": request_id,
-                    }
-                )
+        try:
+            while True:
+                request = None
+                try:
+                    request = self._get_next_request()
+                    if request is None:
+                        self._logger.info("Empty request: exiting")
+                        break
+                    response = self._process_request(request)
+                    if response:  # May be none for cancel request
+                        self._send_response(response)
+                except Exception:
+                    self._logger.exception("Unhandled error: request=%s", request)
+                    output = (
+                        f"Unhandled error:\nRequest: {request}\n"
+                        + traceback.format_exc()
+                    )
+                    request_id = 0 if not request else request.get("requestId", 0)
+                    self._send_response(
+                        {
+                            "exitCode": 3,
+                            "output": output,
+                            "requestId": request_id,
+                        }
+                    )
+        finally:
+            self._logger.info("Worker shutting down")
 
     def _get_next_request(self) -> "object | None":
         line = self._instream.readline()
